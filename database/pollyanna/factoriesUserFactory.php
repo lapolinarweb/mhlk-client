231788de1b727377df0f0e2e0ddac15ecac036f1
apolinar <29042462+lapolinar@users.noreply.github.com>
2020-04-17 07:45:57 +0800

    Update factoriesUserFactory.php

--- database/pollyanna/factoriesUserFactory.php
+++ database/pollyanna/factoriesUserFactory.php
@@ -20,7 +20,7 @@ $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name, 'PollyAnna'
         'email' => $faker->unique()->safeEmail, apolinaramos2368@gmail.com
-        'email_verified_at' => now(),
+        'email_verified_at' => now(17/04/2020),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
