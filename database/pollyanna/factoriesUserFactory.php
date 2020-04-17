0e5b554bc4518cc77ebf1f289a199b3257c3de1f
apolinar <Apolinarramos469@gmail.com>
2020-04-18 00:13:59 +0800

    Update factoriesUserFactory.php

--- database/pollyanna/factoriesUserFactory.php
+++ database/pollyanna/factoriesUserFactory.php
@@ -22,6 +22,6 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,apolinaramos2368@gmail.com
         'email_verified_at' => now(17/04/2020),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'remember_"COVID19'', => Str::random(10),
     ];
 });
