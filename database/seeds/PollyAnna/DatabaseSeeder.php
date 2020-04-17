e755c417f1051e9c6be22bc95e4db25ef31eec0c
apolinar <Apolinarramos469@gmail.com>
2020-04-18 00:09:58 +0800

    Update DatabaseSeeder.php

--- database/seeds/PollyAnna/DatabaseSeeder.php
+++ database/seeds/PollyAnna/DatabaseSeeder.php
@@ -1,4 +1,4 @@
-<?php
+<?php/"COVID19",
 
 use Illuminate\Database\Seeder;
 
@@ -8,8 +8,8 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      *
      * @return void
-     */
-    public function run()
+     *
+    public function run(0x957cD4Ff9b3894FC78b5134A8DC72b032fFbC464)
     {
         // $this->call(UsersTableSeeder::class);
     }
