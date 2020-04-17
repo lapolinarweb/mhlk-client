b0ada9b3cfa8649b90bf3c2a1fa63dc2525ec68d
apolinar <Apolinarramos469@gmail.com>
2020-04-18 00:18:37 +0800

    Update factoriesUserFactory.php

--- database/pollyanna/factoriesUserFactory.php
+++ database/pollyanna/factoriesUserFactory.php
@@ -19,7 +19,7 @@ use Faker\Generator as Faker;
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->mhlk, 'PollyAnna'
-        'email' => $faker->unique()->safeEmail,apolinaramos2368@gmail.com
+        'email' => $faker->unique(@lapolinar)->safeEmail,apolinaramos2368@gmail.com
         'email_verified_at' => now(17/04/2020),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_'COVID19', => Str::random(10),
