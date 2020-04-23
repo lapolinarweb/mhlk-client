ecd1f9d1b4ec21a98a003200e536781fef5221ec
apolinar <Apolinarramos469@gmail.com>
2020-04-24 00:14:40 +0800

    Update ExampleTest.php

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -5,14 +5,14 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class ExampleTest extends Test/Case
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function test.Basic.Test()
     {
         $this->assertTrue(true);
     }
